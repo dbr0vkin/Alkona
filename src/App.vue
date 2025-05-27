483377a4209b54bfbaa73214c31af9f12c1916d3
Alkona <d_brovkin310@mail.ru>
2025-05-27 10:59:13 +0300

    feat: Добавил форму авторизации

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import SignForm from '@/components/SignForm.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <SignForm></SignForm>
   </main>
+<footer>
+
+</footer>
 </template>
 
 <style scoped>
---
+++ src/components/SignForm.vue
@@ -0,0 +1,47 @@
+<script setup>
+import {ref} from 'vue';
+
+const login = ref();
+const password = ref();
+const success = ref(false);
+const errors = ref();
+const clickHadler = () => {
+    if(login.value == null || password == null){
+        errors.value = "Пароль и логин обязательны";
+        return;
+    }
+    errors.value = undefined;
+    success.value = true;
+}
+</script>
+<template>
+<form class="sign-form" >
+    <template v-if="!success">
+        <input type="text" placeholder="Логин" v-model="login"/>
+        <input type="password" placeholder="Пароль" v-model="password"/>
+        <button type="button" @click="clickHadler"></button>
+        <div v-if="errors != null" class="error--text">{{ errors }}</div>
+
+    </template>
+    <div v-else>
+        {{ login }}, Вы, успешно, авторизовались!
+    </div>
+</form>
+</template>
+<style>
+.sign-form{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: auto
+}
+.sign-form{
+    height: 25px;
+    width: 50px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.error--text{
+    color: red;
+}
+</style>
